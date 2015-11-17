ce597ea077a9c952759ffc9f3067ffa59dc6d6d1
FedericoKCL <fed.deployment@aol.co.uk>
2015-11-17 23:27:42 +0000

    MVP website ready

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,19 @@
 exports.home = function(req, res){
   res.render('default', 
-    {title: 'Home',
-     classname: 'home',
-     users: ['Jon', 'Jack', 'Lurido', 'Valerio']
-    });
+             {title: 'Home'});
 }
 
 exports.about = function(req, res){
   res.render('default', 
-    {title: 'About me',
-     classname: 'about',
-    });
+             { title: 'About me'});
 }
 
 exports.projects = function(req, res){
   res.render('default',
-    { title: 'Projects',
-      classname: 'projects'
-    });
+             { title: 'Projects'});
 }
 
 exports.contact = function(req, res){
   res.render('default', 
-    { title: 'Contact me',
-      classname: 'contact'
-    });
+             { title: 'Contact me'});
 }
